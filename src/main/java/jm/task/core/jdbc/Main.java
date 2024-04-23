0191b0691a68e0e83e8fce6ec01dd005f43ddf7d
a700ac <a700ac@gmail.com>
2024-04-23 21:34:24 +0300

    ToCheckup3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -15,24 +15,20 @@ public class Main {
 
         UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
-//        userDaoHibernate.createUsersTable();
-//
-//        List<User> arr = new ArrayList<>();
-//        arr.add(new User("Name1", "LastName1", (byte) 20));
-//        arr.add(new User("Name2", "LastName2", (byte) 25));
-//        arr.add(new User("Name3", "LastName3", (byte) 31));
-//        arr.add(new User("Name4", "LastName4", (byte) 38));
-//        for (User user: arr) {
-//            userDaoHibernate.saveUser(user.getName(), user.getLastName(), user.getAge());
-//            System.out.println("User с именем – " + user.getName() + " добавлен в базу данных");
-//        }
+        userDaoHibernate.createUsersTable();
 
-//      userDaoHibernate.removeUserById(1);
-//
-//      userDaoHibernate.getAllUsers();
-
-
-//      userDaoHibernate.cleanUsersTable();
+        List<User> arr = new ArrayList<>();
+        arr.add(new User("Name1", "LastName1", (byte) 20));
+        arr.add(new User("Name2", "LastName2", (byte) 25));
+        arr.add(new User("Name3", "LastName3", (byte) 31));
+        arr.add(new User("Name4", "LastName4", (byte) 38));
+        for (User user: arr) {
+            userDaoHibernate.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.println("User с именем – " + user.getName() + " добавлен в базу данных");
+        }
+      userDaoHibernate.removeUserById(1);
+      userDaoHibernate.getAllUsers();
+      userDaoHibernate.cleanUsersTable();
       userDaoHibernate.dropUsersTable();
 
     }
